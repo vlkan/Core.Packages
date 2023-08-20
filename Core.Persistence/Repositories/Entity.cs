1502047a970182bb9c054171420229b292bf0dd1
vlkan <thkvolkanonder@gmail.com>
2023-08-20 23:50:21 +0300

    Add project files.

---
+++ Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
+    public Entity()
+    {
+        Id = default;
+    }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
