cd837b5ee4440c6c23102142743e0bf7f501b791
vlkan <thkvolkanonder@gmail.com>
2024-03-18 00:01:07 +0300

    pagerequest

---
+++ Core.Application/Responses/GetListResponse.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Paging;
+
+namespace Core.Application.Responses;
+
+public class GetListResponse<T> : BasePageablaModel
+{
+    private IList<T> _items;
+
+    public IList<T> Items 
+    { 
+        get => _items ?? new List<T>(); 
+        set => _items = value;
+    }
+}
