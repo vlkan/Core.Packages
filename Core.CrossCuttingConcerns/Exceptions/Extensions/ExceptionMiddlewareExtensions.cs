ee4edbf48a58ac114d4fd69f1bf13ac0449ac82f
vlkan <thkvolkanonder@gmail.com>
2025-07-29 23:30:18 +0300

    core error middleware

---
+++ Core.CrossCuttingConcerns/Exceptions/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extensions;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        => app.UseMiddleware<ExceptionMiddleware>();
+}
---
+++ Core.CrossCuttingConcerns/Exceptions/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly HttpExceptionHandler _httpExceptionHandler;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+        _httpExceptionHandler = new HttpExceptionHandler();
+    }
+
+    public async Task Invoke(HttpContext httpContent) 
+    {
+        try
+        {
+            await _next(httpContent);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContent.Response, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpResponse response, Exception exception)
+    {
+        response.ContentType = "application/json";
+        _httpExceptionHandler.Response = response;
+        return _httpExceptionHandler.HandleExceptionAsync(exception);
+    }
+}
