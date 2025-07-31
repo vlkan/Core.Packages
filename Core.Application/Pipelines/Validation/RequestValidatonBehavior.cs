1955af7c41f8f21d449a7816af051626b2f95219
vlkan <thkvolkanonder@gmail.com>
2025-07-31 23:12:41 +0300

    pipeline implementation

---
+++ Core.Application/Pipelines/Validation/RequestValidatonBehavior.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Core.Application.Pipelines.Validation;
+
+public class RequestValidatonBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        ValidationContext<object> context = new(request);
+
+
+    }
+}
